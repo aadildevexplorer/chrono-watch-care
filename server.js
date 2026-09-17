66465e6e7e2cbc1d97f5e3789142b6797bf5f50f
aadildevexplorer <aadilkhan04610461@gmail.com>
2026-09-17 14:46:46 +0530

    Update backend

---
+++ server.js
@@ -0,0 +1,58 @@
+const dns = require("node:dns");
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+const express = require("express");
+const connectDB = require("./config/db_config");
+const errorHandler = require("./middleware/errorHandler");
+require("dotenv").config();
+
+const app = express();
+
+// connect DATABASE
+connectDB();
+
+const PORT = process.env.PORT || 2000;
+
+const cors = require("cors");
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://chronowatchcare.vercel.app",
+      "https://chrono-watch-care-production-7a92.up.railway.app",
+    ],
+  })
+);
+// app.use(
+//   cors({
+//     origin: "*",
+//   }),
+// );
+// body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({
+    msg: "welcome to the ChronoWatchCare",
+  });
+});
+
+// User route
+
+app.use("/api/user", require("./routes/userRoute"));
+
+// Service route
+
+app.use("/api/service", require("./routes/watchServiceRoute"));
+
+// Admin route
+
+app.use("/api/admin", require("./routes/adminRoutes"));
+
+// error Handler
+
+app.use(errorHandler);
+
+app.listen(PORT, (req, res) => {
+  console.log(`I am Server at PORT  : ${PORT}`);
+});
